9ba2fc6c23b4dfbc6619365e8c0d602874c47a68
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2025-08-05 21:05:28 +0200

    chore: removed wrong dir

--- backend/todos/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TodosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'todos'
-    verbose_name = 'Todo Management'

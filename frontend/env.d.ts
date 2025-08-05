d443445917c56323f5b3e319c54025bc6d0b9732
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2025-08-05 21:02:23 +0200

    test commit

--- frontend/env.d.ts
+++
@@ -1,6 +0,0 @@
-// General typings for modules without types
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
